2b682bba5962cec47da242e775e5c09f3acb30f2
Soham Jadia <jadiasoham@gmail.com>
2024-04-29 21:02:02 +0530

    Added one method and changed name of one method

--- MatrixCalculator.h
+++ MatrixCalculator.h
@@ -7,7 +7,8 @@ class MatrixCalculator {
 public:
     std::vector<std::vector<int>> addMatrices(const Matrix& A, const Matrix& B);
     std::vector<std::vector<int>> subtractMatrices(const Matrix& A, const Matrix& B);
-    std::vector<std::vector<int>> multiplyMatrices(const Matrix& A, const Matrix& B);
+    std::vector<std::vector<int>> matrixProduct(const Matrix& A, const Matrix& B);
+    std::vector<std::vector<int>> dotProduct(const Matrix& A, const Matrix& B);
     double determinant(const Matrix& A);
 };
 
